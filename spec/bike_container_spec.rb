78464e419ec1960127ea6832599969ba3f256dbc
BobRazoswki <bobongithub@gmail.com>
2014-07-09 16:20:39 +0100

    boris working just before exercise

---
+++ spec/bike_container_spec.rb
@@ -0,0 +1,48 @@
+require './lib/bike_container'
+
+class ContainerHolder; include BikeContainer; end
+
+describe 'ContainerHolder' do
+
+	let(:holder) {ContainerHolder.new} 
+	let(:bike) {double :bike}
+	let(:bike2) {double :bike}
+
+	def fill_station
+		20.times {holder.dock(bike)}
+	end
+
+	it 'should accept a bike' do
+		expect(holder.bike_count).to eq (0)
+		holder.dock(bike)
+		expect(holder.bike_count).to eq (1)
+	end
+
+	it "should release a bike" do
+		holder.dock(bike)
+		holder.dock(bike2)
+		holder.release(bike2)
+		expect(holder.bikes).to eq ([bike])
+	end
+
+	it "know when it is full" do
+		expect(holder).not_to be_full
+		fill_station
+		expect(holder).to be_full
+	end
+
+	it 'should raise an error message' do
+		fill_station
+		expect{holder.dock(bike)}.to raise_error(RuntimeError)
+	end
+
+	it 'should know when the bike is available or not' do
+		working = double bike, broken?: false
+		broken = double bike, broken?: true
+		holder.dock(working)
+		holder.dock(broken)
+		expect(holder.available_bikes).to eq [working]
+	end
+
+
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+
+describe 'DockingStation' do
+	let(:station) {DockingStation.new(:capacity => 20)}
+
+	it 'should allow the docking station (when initialize) to have a default capacity' do |variable|
+		expect(station.capacity).to eq 20	
+	end
+end
